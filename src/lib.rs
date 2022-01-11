cd7eedd30e91df912a157416645802f6578ebe47
tsabelmann <tim.sabelmann@hotmail.de>
2022-01-11 23:10:02 +0100

    feat: Added modules to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,7 @@ pub mod decode;
 pub mod encode;
 pub mod prelude;
 pub mod mask;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod io;
+mod peak;
+mod signals;
+pub mod endian;
