f6199f77076681e73b3091a9ec08f70b753f4384
tsabelmann <tim.sabelmann@hotmail.de>
2022-01-11 22:54:25 +0100

    feat: Implemented Endian enum

---
+++ src/endian.rs
@@ -0,0 +1,7 @@
+/// Defines the two endiannesses that are considered for decoding and encoding.
+pub enum Endian {
+    /// Defines data to be in little-endian byte-order.
+    Little,
+    /// Defines data to be in big-endian byte-order.
+    Big
+}
