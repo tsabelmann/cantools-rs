cdaca924a8bcc44cb46afd35c2ad1bd6b3d7b4ea
tsabelmann <tim.sabelmann@hotmail.de>
2022-01-12 11:50:34 +0100

    feat: Implemented Debug and Default for Endian

--- src/endian.rs
+++ src/endian.rs
@@ -1,7 +1,14 @@
 /// Defines the two endiannesses that are considered for decoding and encoding.
+#[derive(Debug)]
 pub enum Endian {
     /// Defines data to be in little-endian byte-order.
     Little,
     /// Defines data to be in big-endian byte-order.
     Big
+}
+
+impl Default for Endian {
+    fn default() -> Self {
+        Endian::Little
+    }
 }
