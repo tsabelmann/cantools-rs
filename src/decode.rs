48b1ee28cbb59eb7d89fdba3fdab9af725b54cb0
tsabelmann <tim.sabelmann@hotmail.de>
2022-01-16 17:28:58 +0100

    feat: Added Decode trait

--- src/decode.rs
+++ src/decode.rs
@@ -3,7 +3,22 @@ use crate::data::CANData;
 pub trait TryDecode<T> {
     type Error;
 
-    fn try_decode<D: CANData>(&self, _data: &D) -> Result<T, Self::Error>;
+    fn try_decode<D: CANData>(&self, data: &D) -> Result<T, Self::Error>;
+}
+
+pub trait Decode<T> : TryDecode<T> {
+    type Error;
+
+    fn decode<D: CANData>(&self, data: &D) -> T {
+        match self.try_decode(data) {
+            Ok(value) => {
+                value
+            },
+            Err(_) => {
+                panic!("cannot decode data using");
+            }
+        }
+    }
 }
 
 
