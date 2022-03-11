ce4948ecb724af51a58bc7df47406f9247e06c07
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-12 00:09:02 +0100

    fix: Corrected paths to log files

--- tests/candump.rs
+++ tests/candump.rs
@@ -2,16 +2,24 @@ use cantools::logs::{CANDumpLog, CANDumpLogEntry};
 
 #[test]
 fn candump_log_empty() {
-    let candump = CANDumpLog::open("candumps/logs/empty.log").unwrap();
+    let candump = CANDumpLog::open("candump/logs/empty.log").unwrap();
     let mut iterator = candump.into_iter();
     assert_eq!(iterator.next(), None);
 }
 
 #[test]
-fn candump_log_once() {
-    let candump = CANDumpLog::open("candumps/logs/once.log").unwrap();
+fn candump_log_once_001() {
+    let candump = CANDumpLog::open("candump/logs/once.log").unwrap();
     let mut iterator = candump.into_iter();
     assert_eq!(iterator.next(), Some(CANDumpLogEntry::new(1647037105.079609, "vcan0", 0x42, &[0x12], None)));
+    assert_eq!(iterator.next(), None);
+}
+
+#[test]
+fn candump_log_once_002() {
+    let candump = CANDumpLog::open("candump/logs/once_1.log").unwrap();
+    let mut iterator = candump.into_iter();
+    assert_eq!(iterator.next(), Some(CANDumpLogEntry::new(1647037105.079609, "vcan0", 0x42, &[0x12], Some(0xA))));
     assert_eq!(iterator.next(), None);
 }
 
