62ad411442a6b18a3e16b1a19ab254c41980c276
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-12 00:04:11 +0100

    refact: Changed includes

--- tests/candump.rs
+++ tests/candump.rs
@@ -1,7 +1,17 @@
-use cantools;
+use cantools::io::logs::{CANDumpLog, CANDumpLogEntry};
 
 #[test]
-fn it_adds_two() {
+fn candump_log_empty() {
+    let candump = CANDumpLog::open("candumps/logs/empty.log").unwrap();
+    let mut iterator = candump.into_iter();
+    assert_eq!(iterator.next(), None);
+}
 
+#[test]
+fn candump_log_once() {
+    let candump = CANDumpLog::open("candumps/logs/once.log").unwrap();
+    let mut iterator = candump.into_iter();
+    assert_eq!(iterator.next(), Some(CANDumpLogEntry::new(1647037105.079609, "vcan0", 0x42, &[0x12], None)));
+    assert_eq!(iterator.next(), None);
 }
 
