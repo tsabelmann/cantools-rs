c98a3f4fe55e3d5848154312ab6d390bf62641e8
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-18 19:57:20 +0100

    feat: Added utils module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,25 @@
 #![allow(dead_code)]
 
+pub mod utils;
+
 pub mod data;
+
+#[cfg(feature = "decode")]
 pub mod decode;
+
+#[cfg(feature = "encode")]
 pub mod encode;
+
+#[cfg(feature = "prelude")]
 pub mod prelude;
+
 pub mod mask;
-mod peak;
-mod signals;
+
+#[cfg(feature = "peak")]
+pub mod peak;
+
+#[cfg(feature = "signals")]
+pub mod signals;
+
 pub mod endian;
 pub mod logs;
