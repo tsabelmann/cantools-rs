d5889912702ea19a1ac57240cea6e527cfe63619
tsabelmann <tim.sabelmann@hotmail.de>
2022-03-18 20:22:18 +0100

    feat: Implemented file formats

--- src/logs.rs
+++ src/logs.rs
@@ -10,16 +10,6 @@ pub struct CANDump {
     file: File
 }
 
-pub struct CANDumpEntry {
-    interface: String,
-    can_id: u32,
-    data: Vec<u8>
-}
-
-pub struct CANDumpIterator {
-    lines: Lines<BufReader<File>>
-}
-
 impl CANDump {
     pub fn open<P>(path: P) -> io::Result<CANDump>
     where
@@ -34,15 +24,78 @@ impl CANDump {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub struct CANDumpEntry {
+    interface: String,
+    can_id: u32,
+    data: Vec<u8>
+}
+
+impl CANData for CANDumpEntry {
+    fn data(&self) -> &[u8] {
+        &self.data
+    }
+
+    fn dlc(&self) -> usize {
+        self.data.len()
+    }
+}
+
+impl CANDumpEntry {
+    pub fn new(interface: &str, can_id:u32, data: Vec<u8>) -> Self {
+        CANDumpEntry {
+            interface: String::from(interface),
+            can_id,
+            data
+        }
+    }
+}
+
+pub struct CANDumpIterator {
+    lines: Lines<BufReader<File>>
+}
+
 impl Iterator for CANDumpIterator {
-    type Item = ();
+    type Item = CANDumpEntry;
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        loop {
+            let line = self.lines.next();
+            match line {
+                Some(Ok(line)) => {
+                    let splits = line
+                        .split(' ')
+                        .collect::<Vec<_>>();
+
+                    let interface = match splits.get(0).copied() {
+                        Some(interface) => interface,
+                        None => continue
+                    };
+
+                    let can_id = match splits
+                        .get(1).copied().map(|e| u32::from_str_radix(e, 16)) {
+                        Some(Ok(can_id)) => can_id,
+                        _ => continue
+                    };
+
+                    let mut data = Vec::new();
+                    for entry in splits.into_iter().skip(3) {
+                        match u8::from_str_radix(entry, 16) {
+                            Ok(u8_entry) => data.push(u8_entry),
+                            _ => continue
+                        }
+                    }
+
+                    return Some(CANDumpEntry::new(interface, can_id, data));
+                },
+                Some(Err(_)) => continue,
+                None => return None
+            }
+        }
     }
 }
 
 impl IntoIterator for CANDump {
-    type Item = ();
+    type Item = CANDumpEntry;
     type IntoIter = CANDumpIterator;
     fn into_iter(self) -> Self::IntoIter {
         CANDumpIterator {
@@ -55,6 +108,20 @@ pub struct CANDumpLog {
     file: File
 }
 
+impl CANDumpLog {
+    pub fn open<P>(path: P) -> io::Result<CANDumpLog>
+    where
+        P: AsRef<Path>
+    {
+        let file = File::open(path)?;
+        Ok(CANDumpLog {file})
+    }
+
+    pub fn into_inner(self) -> File {
+        self.file
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct CANDumpLogEntry {
     timestamp: f64,
@@ -65,12 +132,12 @@ pub struct CANDumpLogEntry {
 }
 
 impl CANDumpLogEntry {
-    pub fn new(timestamp: f64, interface: &str, can_id:u32, data: &[u8], flag: Option<u8>) -> Self {
+    pub fn new(timestamp: f64, interface: &str, can_id:u32, data: Vec<u8>, flag: Option<u8>) -> Self {
         CANDumpLogEntry {
             timestamp,
             interface: String::from(interface),
             can_id,
-            data: Vec::<_>::from(data),
+            data,
             flag
         }
     }
@@ -90,20 +157,6 @@ pub struct CANDumpLogIterator {
     lines: Lines<BufReader<File>>
 }
 
-impl CANDumpLog {
-    pub fn open<P>(path: P) -> io::Result<CANDumpLog>
-    where
-        P: AsRef<Path>
-    {
-        let file = File::open(path)?;
-        Ok(CANDumpLog {file})
-    }
-
-    pub fn into_inner(self) -> File {
-        self.file
-    }
-}
-
 impl Iterator for CANDumpLogIterator {
     type Item = CANDumpLogEntry;
     fn next(&mut self) -> Option<Self::Item> {
@@ -112,7 +165,7 @@ impl Iterator for CANDumpLogIterator {
             match line {
                 Some(Ok(line)) => {
                     if let [timestamp_string, interface, can_id_data_string] = line
-                        .split(" ")
+                        .split(' ')
                         .take(3)
                         .collect::<Vec<_>>()[..]
                     {
@@ -124,7 +177,7 @@ impl Iterator for CANDumpLogIterator {
                         };
 
                         if let [can_id_string, _, data_string] = can_id_data_string
-                            .split("#")
+                            .split('#')
                             .take(3)
                             .collect::<Vec<_>>()[..]
                         {
@@ -144,11 +197,11 @@ impl Iterator for CANDumpLogIterator {
                                 data.push(value);
                             }
 
-                            return Some(Self::Item::new(timestamp, interface, can_id, &data, Some(flag)));
+                            return Some(Self::Item::new(timestamp, interface, can_id, data, Some(flag)));
                         }
 
                         if let [can_id_string, data_string] = can_id_data_string
-                            .split("#")
+                            .split('#')
                             .take(2)
                             .collect::<Vec<_>>()[..]
                         {
@@ -162,7 +215,7 @@ impl Iterator for CANDumpLogIterator {
                                 data.push(u8::from_str_radix(&data_string[2*i..2*i+2], 16).unwrap())
                             }
 
-                            return Some(Self::Item::new(timestamp, interface, can_id, &data, None));
+                            return Some(Self::Item::new(timestamp, interface, can_id, data, None));
                         }
                     }
                 },
