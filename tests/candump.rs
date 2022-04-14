356054976f88611a09bb9e347bea87ecd3c70bf0
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-14 21:20:49 +0200

    feat: Changed tests

--- tests/candump.rs
+++ tests/candump.rs
@@ -32,14 +32,12 @@ fn can_dump_raw_once_2() {
 #[test]
 fn can_dump_raw_parse_1() {
     let entry = "can0 00001337 [1] 01".parse::<CANDumpEntry>();
-    assert_eq!(entry.is_ok(), true);
-    assert_eq!(entry.unwrap(), CANDumpEntry::new("can0",0x1337,vec![0x01]));
+    assert_eq!(entry, Ok(CANDumpEntry::new("can0",0x1337,vec![0x01])));
 }
 
 #[test]
 fn can_dump_raw_parse_2() {
     let entry = "can0 00001337 [2] 01".parse::<CANDumpEntry>();
-    assert_eq!(entry.is_err(), true);
     assert_eq!(entry, Err(CANDumpEntryParseError::DlcDataMismatch));
 }
 
