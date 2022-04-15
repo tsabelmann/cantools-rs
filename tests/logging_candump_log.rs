9c3b5ef67888c7eb1d9719de01e01840aa19fcfd
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-15 14:42:56 +0200

    refact: Renamed file

---
+++ tests/logging_candump_log.rs
@@ -0,0 +1,33 @@
+use cantools::logging::{CANDumpLog, CANDumpLogEntry};
+
+#[test]
+fn can_dump_log_empty() {
+    let candump = CANDumpLog::open("candump/logs/empty.log").unwrap();
+    let mut iterator = candump.into_iter();
+    assert_eq!(iterator.next(), None);
+}
+
+#[test]
+fn can_dump_log_once_001() {
+    let candump = CANDumpLog::open("candump/logs/once_1.log").unwrap();
+    let mut iterator = candump.into_iter();
+    assert_eq!(iterator.next(), Some(CANDumpLogEntry::new(1647037105.079609,
+                                                          "vcan0",
+                                                          0x42,
+                                                          vec![0x12],
+                                                          None).unwrap()));
+    assert_eq!(iterator.next(), None);
+}
+
+#[test]
+fn can_dump_log_once_002() {
+    let candump = CANDumpLog::open("candump/logs/once_2.log").unwrap();
+    let mut iterator = candump.into_iter();
+    assert_eq!(iterator.next(), Some(CANDumpLogEntry::new(1647037105.079609,
+                                                          "vcan0",
+                                                          0x42,
+                                                          vec![0x12],
+                                                          Some(0xA)).unwrap()));
+    assert_eq!(iterator.next(), None);
+}
+
