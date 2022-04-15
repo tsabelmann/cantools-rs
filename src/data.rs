d7aee0c7ec8bd557273bda99f74be327ce6e15b5
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-15 15:13:22 +0200

    docs: Added module documentation

--- src/data.rs
+++ src/data.rs
@@ -1,36 +1,66 @@
-//! DATA MODULE
+//! Module providing convenient access to the underlying CAN-bus data, either read-only
+//! (see [CANData]), or read and write (see [CANWrite]).
+//!
+//! The [CANData] trait provides methods and retrieve the [DLC](CANData::dlc), the number of
+//! available bytes, and a [data](CANData::data) slice that is read-only. In theory, only the
+//! [data](CANData::data) slice is needed since one can retrieve the DLC from the slice as well.
+//!
+//! The [CANWrite] trait provides additional methods. The [mut_data](CANWrite::mut_data) method
+//! allows for mutating the slice. [set_and_exchange](CANWrite::set_and_exchange) and
+//! [set](CANWrite::set) allow for safe and unsafe ways to change specific bytes.
 
-
-/// TRAIT
+/// A trait providing methods for accessing the underlying bytes of some CAN-bus data.
 pub trait CANData {
-    /// Return data
+    /// Returns a slice representing the accessible bytes.
     fn data(&self) -> &[u8];
 
-    /// Returns the length of the slize
+    /// Returns the number of accessible bytes.
     fn dlc(&self) -> usize;
 }
 
-impl CANData for Vec<u8> {
+/// Type representing failure in the process of writing CAN-bus data.
+#[derive(Debug, PartialEq)]
+pub enum CANWriteError {
+    /// The byte at some index is not available or not existent.
+    UnavailableByte(u8)
+}
+
+/// A trait providing methods for accessing the underlying data in a mutable fashion.
+pub trait CANWrite: CANData {
+    /// Returns a mutable slice representing the mutable data.
+    fn mut_data(&mut self) -> &mut [u8];
+
+    /// Retrieved the byte and `index` ans sets it to the value `value`. If the byte at `index` is
+    /// unavailable, a [CANWriteError] is returned.
+    fn set_and_exchange(&mut self, value: u8, index: u8) -> Result<u8, CANWriteError>;
+
+    /// Retrieved the byte and `index` and sets it to the value `value`. Uses
+    /// [set_and_exchange](CANWrite::set_and_exchange) underneath.
     ///
+    /// # Panics
+    /// If the byte at `index` is unavailable or non existent, the method panics.
+    fn set(&mut self, value: u8, index: u8) -> u8 {
+        self.set_and_exchange(value, index).unwrap()
+    }
+}
+
+impl CANData for Vec<u8> {
     /// # Example
     /// ```
     /// use cantools::data::CANData;
     /// let v = Vec::<u8>::new();
     /// assert_eq!(CANData::data(&v), &[]);
     /// ```
-    ///
     fn data(&self) -> &[u8] {
         self.as_slice()
     }
 
-    ///
     /// # Example
     /// ```
     /// use cantools::data::CANData;
     /// let v = Vec::<u8>::new();
     /// assert_eq!(CANData::dlc(&v), 0);
     /// ```
-    ///
     fn dlc(&self) -> usize {
         self.len()
     }
