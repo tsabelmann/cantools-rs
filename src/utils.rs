53802e44a93ce60a5eaac79b2db210c00a09b8ad
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-15 12:38:18 +0200

    refact: Moved endian.rs and mask.rs into utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -233,6 +233,20 @@ impl Mask for i64 {
     }
 }
 
+#[derive(Debug,PartialEq)]
+pub enum Endian {
+    /// Defines data to be in little-endian byte-order.
+    Little,
+    /// Defines data to be in big-endian byte-order.
+    Big
+}
+
+impl Default for Endian {
+    fn default() -> Self {
+        Endian::Little
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Mask;
