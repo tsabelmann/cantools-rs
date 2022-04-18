e923b4d8831ddb526e2e80612a6b3b301f86615c
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-18 12:14:27 +0200

    docs: Added documentation

--- src/decode.rs
+++ src/decode.rs
@@ -1,26 +1,47 @@
-//! Module providing utility traits for extracting, i.e., decoding CAN-bus data.
+//! Module providing utility traits for decoding CAN-bus data.
 //!
 //! The module provides three traits: [TryDecode], [DefaultDecode], and [Decode].
+//! [try_decode](TryDecode::try_decode) models the possibility of the decoding to fail.
+//! [default_decode](DefaultDecode::default_decode) returns a default value if the decoding fails.
+//! Finally, [decode](Decode::decode) panics if the internal decoding fails. Otherwise, it returns
+//! the decoded value.
+//!
+//! # Example
+//! ```
+//! use cantools::data::CANData;
+//! use cantools::signals::Bit;
+//! use cantools::decode::{TryDecode, DefaultDecode, Decode};
+//!
+//! let bit = Bit::new(20);
+//! let mut data = [1u8, 2u8, 3u8, 4u8];
+//!
+//! let result_1 = bit.try_decode(&data);
+//! let result_2 = bit.default_decode(&data);
+//! let result_3 = bit.decode(&data);
+//! ```
 
 use crate::data::CANData;
 
-///
+/// Type representing possible decoding errors.
 #[derive(Debug, PartialEq)]
 pub enum DecodeError {
-    ///
+    /// There is not enough byte data available from which one can decode a value.
     NotEnoughData
 }
 
-///
+/// A trait modeling the failable decoding of data.
 pub trait TryDecode<T> {
-    ///
+    /// A type modeling the different possible failures of the decoding.
     type Error;
 
-    ///
+    /// Tries to decode a value.
     fn try_decode<D: CANData>(&self, data: &D) -> Result<T, Self::Error>;
 }
 
+/// A trait modeling the failable decoding of data.
 pub trait DefaultDecode<T: Default>: TryDecode<T> {
+    /// Tries to decode a value. If the data is not decodable, a default value is returned.
+    /// Otherwise, the decoded value is returned.
     fn default_decode<D: CANData>(&self, data: &D) -> T {
         match self.try_decode(data) {
             Ok(value) => value,
@@ -29,7 +50,12 @@ pub trait DefaultDecode<T: Default>: TryDecode<T> {
     }
 }
 
+/// A trait modeling the not failable decoding of data.
 pub trait Decode<T> : TryDecode<T> {
+    /// Decodes the data.
+    ///
+    /// # Panics
+    /// If the data is not decodable internally, the call to [decode](Decode::decode) panics.
     fn decode<D: CANData>(&self, data: &D) -> T {
         match self.try_decode(data) {
             Ok(value) => value,
