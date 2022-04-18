b2f19d9af1e3a2bd74dafc7d8ee684b9f50217ba
tsabelmann <tim.sabelmann@hotmail.de>
2022-04-18 15:43:01 +0200

    style: Changed styling

--- tests/logging_candump_raw.rs
+++ tests/logging_candump_raw.rs
@@ -4,6 +4,7 @@ use cantools::logging::{CANDump, CANDumpEntry, CANDumpEntryParseError};
 fn can_dump_raw_empty() {
     let candump = CANDump::open("candump/raw/empty.log").unwrap();
     let mut iterator = candump.into_iter();
+
     assert_eq!(iterator.next(), None);
 }
 
@@ -12,9 +13,10 @@ fn can_dump_raw_once_1() {
     let candump = CANDump::open("candump/raw/once_1.log").unwrap();
     let mut iterator = candump.into_iter();
     let vec = vec![0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08];
-    assert_eq!(iterator.next(), Some(CANDumpEntry::new("vcan0",
-                                                          0x1337,
-                                                          vec).unwrap()));
+    assert_eq!(
+        iterator.next(),
+        Some(CANDumpEntry::new("vcan0", 0x1337, vec).unwrap())
+    );
     assert_eq!(iterator.next(), None);
 }
 
@@ -23,16 +25,20 @@ fn can_dump_raw_once_2() {
     let candump = CANDump::open("candump/raw/once_2.log").unwrap();
     let mut iterator = candump.into_iter();
     let vec = vec![0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01];
-    assert_eq!(iterator.next(), Some(CANDumpEntry::new("can0",
-                                                          0x1337,
-                                                          vec).unwrap()));
+    assert_eq!(
+        iterator.next(),
+        Some(CANDumpEntry::new("can0", 0x1337, vec).unwrap())
+    );
     assert_eq!(iterator.next(), None);
 }
 
 #[test]
 fn can_dump_raw_parse_1() {
     let entry: CANDumpEntry = "can0 00001337 [1] 01".parse().unwrap();
-    assert_eq!(entry, CANDumpEntry::new("can0",0x1337,vec![0x01]).unwrap());
+    assert_eq!(
+        entry,
+        CANDumpEntry::new("can0", 0x1337, vec![0x01]).unwrap()
+    );
 }
 
 #[test]
